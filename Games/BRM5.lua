79ffd05bc3d3a0cb4fc50794dbfacd075e57c1db
Sanchez1910 <98999420+Sanchez1910@users.noreply.github.com>
2024-04-22 17:21:25 +0700

    Add files via upload

--- Games/BRM5.lua
+++ Games/BRM5.lua
@@ -33,15 +33,15 @@ local GroundTip,AircraftTip,NoClipEvent,NoClipObjects,WhiteColor = nil,nil,nil,{
 local Teleports = {
     {"Forward Operating Base", Vector3.new(-3993,64,757)     },
     {"Communications Tower",   Vector3.new(-1800,785,-4140)  },
-    {"Department Of Utilities",Vector3.new(-54,63,-3645)     },
-    {"Vietnama Village",       Vector3.new(739,118,-92)      },
+    {"Department Of Utilities",Vector3.new(-54,65,-3645)     },
+    {"Vietnama Village",       Vector3.new(739,120,-92)      },
     {"Fort Ronograd",          Vector3.new(6359,192,-1468)   },
-    {"Ronograd City",          Vector3.new(3478,176,1073)    },
-    {"Sochraina City",         Vector3.new(93,26,3630)       },
-    {"El Chara",               Vector3.new(-4768,108,5218)   },
-    {"Naval Docks",            Vector3.new(6174,130,2099)    },
-    {"Quarry",                 Vector3.new(331,86,2598)      },
-    {"Nuclear Silo",           Vector3.new(1024,44,-5148)    }
+    {"Ronograd City",          Vector3.new(3478,178,1073)    },
+    {"Sochraina City",         Vector3.new(93,28,3630)       },
+    {"El Chara",               Vector3.new(-4768,110,5218)   },
+    {"Naval Docks",            Vector3.new(6174,132,2099)    },
+    {"Quarry",                 Vector3.new(331,88,2598)      },
+    {"Nuclear Silo",           Vector3.new(1024,46,-5148)    }
 }
 
 local KnownBodyParts = {
@@ -61,7 +61,7 @@ local Window = Parvus.Utilities.UI:Window({
 }) do
 
     local CombatTab = Window:Tab({Name = "Combat"}) do
-        local AimbotSection = CombatTab:Section({Name = "Aimbot",Side = "Left"}) do
+        --[[local AimbotSection = CombatTab:Section({Name = "Aimbot",Side = "Left"}) do
             AimbotSection:Toggle({Name = "Enabled",Flag = "Aimbot/Enabled",Value = false})
             :Keybind({Flag = "Aimbot/Keybind",Value = "MouseButton2",Mouse = true,DisableToggle = true,
             Callback = function(Key,KeyDown) Aimbot = Window.Flags["Aimbot/Enabled"] and KeyDown end})
@@ -92,7 +92,7 @@ local Window = Parvus.Utilities.UI:Window({
             AFOVSection:Colorpicker({Name = "Color",Flag = "Aimbot/FOVCircle/Color",Value = {1,0.66666662693024,1,0.25,false}})
             AFOVSection:Slider({Name = "NumSides",Flag = "Aimbot/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
             AFOVSection:Slider({Name = "Thickness",Flag = "Aimbot/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
-        end
+        end]]
         local SilentAimSection = CombatTab:Section({Name = "Silent Aim",Side = "Left"}) do
             SilentAimSection:Toggle({Name = "Enabled",Flag = "SilentAim/Enabled",Value = false}):Keybind({Mouse = true,Flag = "SilentAim/Keybind"})
 
@@ -125,7 +125,7 @@ local Window = Parvus.Utilities.UI:Window({
             --SAFOVSection:Slider({Name = "NumSides",Flag = "SilentAim/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
             SAFOVSection:Slider({Name = "Thickness",Flag = "SilentAim/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
         end
-        local TriggerSection = CombatTab:Section({Name = "Trigger",Side = "Right"}) do
+        --[[local TriggerSection = CombatTab:Section({Name = "Trigger",Side = "Right"}) do
             TriggerSection:Toggle({Name = "Enabled",Flag = "Trigger/Enabled",Value = false})
             :Keybind({Flag = "Trigger/Keybind",Value = "MouseButton2",Mouse = true,DisableToggle = true,
             Callback = function(Key,KeyDown) Trigger = Window.Flags["Trigger/Enabled"] and KeyDown end})
@@ -157,7 +157,7 @@ local Window = Parvus.Utilities.UI:Window({
             TFOVSection:Colorpicker({Name = "Color",Flag = "Trigger/FOVCircle/Color",Value = {0.0833333358168602,0.6666666269302368,1,0.25,false}})
             TFOVSection:Slider({Name = "NumSides",Flag = "Trigger/FOVCircle/NumSides",Min = 3,Max = 100,Value = 14})
             TFOVSection:Slider({Name = "Thickness",Flag = "Trigger/FOVCircle/Thickness",Min = 1,Max = 10,Value = 2})
-        end
+        end]]
     end
     local VisualsSection = Parvus.Utilities:ESPSection(Window,"Visuals","ESP/Player",true,true,true,true,true,false) do
         VisualsSection:Colorpicker({Name = "Ally Color",Flag = "ESP/Player/Ally",Value = {0.3333333432674408,0.6666666269302368,1,0,false}})
@@ -478,7 +478,7 @@ local function GetClosest(Enabled,
 
     return Closest
 end
-local function AimAt(Hitbox,Sensitivity)
+--[[local function AimAt(Hitbox,Sensitivity)
     if not Hitbox then return end
     if Window.Flags["Aimbot/Thirdperson"] then
         mousemoverel(Hitbox[3].Position,true,Sensitivity)
@@ -490,7 +490,7 @@ local function AimAt(Hitbox,Sensitivity)
         (Hitbox[4].X - MouseLocation.X) * Sensitivity,
         (Hitbox[4].Y - MouseLocation.Y) * Sensitivity
     ))
-end
+end]]
 
 function RequireModule(Name)
     for Index, Instance in pairs(getloadedmodules()) do
@@ -837,7 +837,7 @@ OldNamecall = hookmetamethod(game,"__namecall",function(Self,...)
     return OldNamecall(Self,...)
 end)
 
-Parvus.Utilities.NewThreadLoop(0,function()
+--[[Parvus.Utilities.NewThreadLoop(0,function()
     if not (Aimbot or Window.Flags["Aimbot/AlwaysEnabled"]) then return end
 
     AimAt(GetClosest(
@@ -852,7 +852,7 @@ Parvus.Utilities.NewThreadLoop(0,function()
         Window.Flags["Aimbot/Prediction"],
         Window.Flags["BRM5/NPCMode"]
     ),Window.Flags["Aimbot/Sensitivity"] / 100)
-end)
+end)]]
 Parvus.Utilities.NewThreadLoop(0,function()
     SilentAim = GetClosest(
         Window.Flags["SilentAim/Enabled"],
